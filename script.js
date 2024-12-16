8f4798dfb64a069795dd8e1d1629e05544d5a49c
Prasansa23 <senuriprasansa20@gmail.com>
2024-12-16 08:02:52 +0530

    final2 commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Full Stack Developer", "Web Developer"],
+    strings: ["Front End Developer", "Web Developer"],
     typeSpeed: 100,
     backSpeed:100,
     backDelay: 1000,
